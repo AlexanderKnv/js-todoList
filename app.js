9734fa667221ae5de671dd5a6fb15628f5d3481f
Konaschkov <konashkov23@gmail.com>
2023-09-25 10:34:47 +0200

    Fetching data from the server, Rendering received tasks

---
+++ app.js
@@ -0,0 +1,71 @@
+const todoList = document.getElementById('todo-list');
+
+let todos = [];
+let users = [];
+
+document.addEventListener('DOMContentLoaded', initApp);
+
+async function getAllTodos() {
+    try {
+        const response = await fetch(
+            'https://jsonplaceholder.typicode.com/todos?_limit=15'
+        );
+        const data = await response.json();
+
+        return data;
+    } catch (error) {
+        alertError(error);
+    }
+}
+
+async function getAllUsers() {
+    try {
+        const response = await fetch(
+            'https://jsonplaceholder.typicode.com/users?_limit=5'
+        );
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        alertError(error);
+    }
+}
+
+function alertError(error) {
+    alert(error.message);
+}
+
+function initApp() {
+    Promise.all([getAllTodos(), getAllUsers()]).then((values) => {
+        [todos, users] = values;
+        console.log(values)
+
+        todos.forEach((todo) => printTodo(todo));
+    });
+}
+
+function printTodo({ id, userId, title, completed }) {
+    const li = document.createElement('li');
+    li.className = 'todo-item';
+    li.dataset.id = id;
+    li.innerHTML = `<span>${title} <i>by</i> <b>${getUserName(
+        userId
+    )}</b></span>`;
+
+    const status = document.createElement('input');
+    status.type = 'checkbox';
+    status.checked = completed;
+
+    const close = document.createElement('span');
+    close.innerHTML = '&times;';
+    close.className = 'close';
+
+    li.prepend(status);
+    li.append(close);
+
+    todoList.prepend(li);
+}
+
+function getUserName(userId) {
+    const user = users.find((u) => u.id === userId);
+    return user.name;
+}
